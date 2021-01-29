b7fee5a41f3adc6aae324face9f545aec8f46f50
DjDeveloperr <djsandhu2005@gmail.com>
2021-01-29 15:03:42 +0530

    forgot to save file lol

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export { EventEmitter } from 'https://deno.land/x/event@0.2.1/mod.ts'
-export { unzlib } from 'https://deno.land/x/denoflate@1.1/mod.ts'
+export { unzlib } from 'https://raw.githubusercontent.com/DjDeveloperr/denoflate/master/mod.ts'
 export { fetchAuto } from 'https://raw.githubusercontent.com/DjDeveloperr/fetch-base64/main/mod.ts'
 export { parse } from 'https://deno.land/x/mutil@0.1.2/mod.ts'
 export { connect } from 'https://deno.land/x/redis@v0.14.1/mod.ts'
