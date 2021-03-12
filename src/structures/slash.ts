5b015c6c080761e3b71a0fbbd5b23ef7369a4670
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2021-03-12 15:01:14 +0530

    fix interaction user

--- src/structures/slash.ts
+++ src/structures/slash.ts
@@ -147,7 +147,9 @@ export class Interaction extends SnowflakeBase {
     const op = this.options.find((e) => e.name === name)
     if (op === undefined || op.value === undefined) return undefined as any
     if (op.type === SlashCommandOptionType.USER)
-      return this.resolved.users[op.value] as any
+      const u: InteractionUser = this.resolved.users[op.value] as any
+      if (this.resolved.members[op.value] !== undefined) u.member = this.resolved.members[op.value] as any
+      return u
     else if (op.type === SlashCommandOptionType.ROLE)
       return this.resolved.roles[op.value] as any
     else if (op.type === SlashCommandOptionType.CHANNEL)
