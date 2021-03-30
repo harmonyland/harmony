b4a1ae389d35d950a7c954cb26c5a332ad4ac129
Helloyunho <yunho050840@gmail.com>
2021-03-31 01:43:01 +0900

    :sparkles: Add some few things that are useful to voiceState

--- src/structures/voiceState.ts
+++ src/structures/voiceState.ts
@@ -1,4 +1,5 @@
 import { Client } from '../models/client.ts'
+import { ChannelTypes } from '../types/channel.ts'
 import { VoiceStatePayload } from '../types/voice.ts'
 import { Base } from './base.ts'
 import { Guild } from './guild.ts'
@@ -52,12 +53,81 @@ export class VoiceState extends Base {
     this.deaf = data.deaf ?? this.deaf
     this.channelID = data.channel_id ?? this.channelID
     this.mute = data.mute ?? this.mute
-    this.deaf = data.self_deaf ?? this.deaf
-    this.mute = data.self_mute ?? this.mute
     this.selfDeaf = data.self_deaf ?? this.selfDeaf
     this.selfMute = data.self_mute ?? this.selfMute
     this.stream = data.self_stream ?? this.stream
     this.video = data.self_video ?? this.video
     this.suppress = data.suppress ?? this.suppress
+  }
+
+  /**
+   * Disconnects a Member from connected VC
+   */
+  async disconnect(): Promise<Member | undefined> {
+    const result = this.member?.disconnectVoice()
+    if (result !== undefined) {
+      this.channelID = null
+      this.channel = null
+    }
+    return result
+  }
+
+  /**
+   * Moves a Member to another VC
+   * @param channel Channel to move(null or undefined to disconnect)
+   */
+  async moveChannel(
+    channel?: string | VoiceChannel | null
+  ): Promise<Member | undefined> {
+    const result = this.member?.moveVoiceChannel(channel)
+    if (result !== undefined) {
+      let channelFetched: VoiceChannel | null
+      let channelID: string | null
+      if (typeof channel === 'string') {
+        channelID = channel
+        const channelCached = await this.guild?.channels.fetch(channel)
+        if (channelCached?.type === ChannelTypes.GUILD_VOICE) {
+          channelFetched = channelCached as VoiceChannel
+        } else {
+          throw new Error(`Channel ${channel} is not a VoiceChannel.`)
+        }
+      } else {
+        channelID = channel?.id ?? null
+        channelFetched = channel ?? null
+      }
+      this.channelID = channelID
+      this.channel = channelFetched
+    }
+    return result
+  }
+
+  /**
+   * Sets a Member mute in VC
+   * @param mute Value to set
+   */
+  async setMute(mute?: boolean): Promise<Member | undefined> {
+    return this.member?.setMute(mute)
+  }
+
+  /**
+   * Sets a Member deaf in VC
+   * @param deaf Value to set
+   */
+  async setDeaf(deaf?: boolean): Promise<Member | undefined> {
+    return this.member?.setDeaf(deaf)
+  }
+
+  /**
+   * Unmutes the Member from VC.
+   */
+  async unmute(): Promise<Member | undefined> {
+    return await this.setMute(false)
+  }
+
+  /**
+   * Undeafs the Member from VC.
+   */
+  async undeaf(): Promise<Member | undefined> {
+    return await this.setDeaf(false)
   }
 }
