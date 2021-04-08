d681dc41a72bf4253c04d1663f7fbc03d6c97361
DjDeveloperr <djsandhu2005@gmail.com>
2021-04-08 14:52:00 +0530

    fix

--- src/client/collectors.ts
+++ src/client/collectors.ts
@@ -1,5 +1,5 @@
 import { Collection } from '../utils/collection.ts'
-import type { Client } from '../client/mod.ts'
+import type { Client } from '../client/client.ts'
 import { HarmonyEventEmitter } from '../utils/events.ts'
 
 export type CollectorFilter = (...args: any[]) => boolean | Promise<boolean>
