dcbf635860b3e1cb91e6558df5353be61ef51d51
mierenmanz <christiaanmens@gmail.com>
2021-04-28 20:28:19 +0200

    formatting

--- test/argsparser_test.ts
+++ test/argsparser_test.ts
@@ -1,73 +1,94 @@
-import { parseArgs, Args } from "../src/utils/command.ts";
-import { assertEquals, assertNotEquals } from "https://deno.land/std@0.95.0/testing/asserts.ts"
+import { parseArgs, Args } from '../src/utils/command.ts'
+import {
+  assertEquals,
+  assertNotEquals
+} from 'https://deno.land/std@0.95.0/testing/asserts.ts'
 
 // debugger
 const commandArgs: Args[] = [
   {
-    name: "permaban",
-    match: "flag",
-    flag: "--permanent",
-    defaultValue: true,
-  }, {
-    name: "user",
-    match: "mention",
-  }, {
-    name: "reason",
-    match: "rest",
-    defaultValue: "ree"
+    name: 'permaban',
+    match: 'flag',
+    flag: '--permanent',
+    defaultValue: true
+  },
+  {
+    name: 'user',
+    match: 'mention'
+  },
+  {
+    name: 'reason',
+    match: 'rest',
+    defaultValue: 'ree'
   }
-];
+]
 
-const messageArgs1: string[] = ["<@!708544768342229012>","--permanent","bye","bye","Skyler"];
+const messageArgs1: string[] = [
+  '<@!708544768342229012>',
+  '--permanent',
+  'bye',
+  'bye',
+  'Skyler'
+]
 const expectedResult1 = {
   permaban: true,
-  user: "708544768342229012",
-  reason: ["bye","bye","Skyler"]
+  user: '708544768342229012',
+  reason: ['bye', 'bye', 'Skyler']
 }
 
 Deno.test({
-  name: "parse command arguments 1 (assertEquals)",
+  name: 'parse command arguments 1 (assertEquals)',
   fn: () => {
-    const result = parseArgs(commandArgs,messageArgs1);
+    const result = parseArgs(commandArgs, messageArgs1)
     assertEquals(result, expectedResult1)
   },
   sanitizeOps: true,
   sanitizeResources: true,
-  sanitizeExit: true,
+  sanitizeExit: true
 })
 
-const messageArgs2: string[] = ["<@!708544768342229012>","bye","bye","Skyler"];
+const messageArgs2: string[] = [
+  '<@!708544768342229012>',
+  'bye',
+  'bye',
+  'Skyler'
+]
 const expectedResult2 = {
   permaban: true,
-  user: "708544768342229012",
-  reason: ["bye","bye","Skyler"]
+  user: '708544768342229012',
+  reason: ['bye', 'bye', 'Skyler']
 }
 
 Deno.test({
-  name: "parse command arguments 2 (assertEquals)",
+  name: 'parse command arguments 2 (assertEquals)',
   fn: () => {
-    const result = parseArgs(commandArgs, messageArgs2);
+    const result = parseArgs(commandArgs, messageArgs2)
     assertEquals(result, expectedResult2)
   },
   sanitizeOps: true,
   sanitizeResources: true,
-  sanitizeExit: true,
+  sanitizeExit: true
 })
 
-const messageArgs3: string[] = ["<@!708544768342229012>","bye","bye","Skyler"];
+const messageArgs3: string[] = [
+  '<@!708544768342229012>',
+  'bye',
+  'bye',
+  'Skyler'
+]
 const expectedResult3 = {
   permaban: false,
-  user: "708544768342229012",
-  reason: ["bye","bye","Skyler"]
+  user: '708544768342229012',
+  reason: ['bye', 'bye', 'Skyler']
 }
 
 Deno.test({
-  name: "parse command arguments 3 (assertNotEquals)",
+  name: 'parse command arguments 3 (assertNotEquals)',
   fn: () => {
-    const result = parseArgs(commandArgs, messageArgs3);
+    const result = parseArgs(commandArgs, messageArgs3)
     assertNotEquals(result, expectedResult3)
   },
   sanitizeOps: true,
   sanitizeResources: true,
-  sanitizeExit: true,
+  sanitizeExit: true
 })
