f0e9dcfc6700a4f3ddc3f6da05fad6949f59688a
DjDeveloperr <djsandhu2005@gmail.com>
2021-05-14 13:19:09 +0530

    improve sharding

---
+++ test/speed.ts
@@ -0,0 +1,23 @@
+import * as discord from '../mod.ts'
+import { TOKEN2 } from './config.ts'
+
+const client = new discord.Client({
+  intents: ['GUILDS'],
+  token: TOKEN2,
+  shardCount: 5
+})
+
+let started: number
+
+client.on('shardReady', (shard) => {
+  console.log(`Shard ${shard} ready!`)
+})
+
+client.once('ready', (shards) => {
+  console.log(`Launched ${shards} shards! Time: ${performance.now() - started}`)
+})
+
+console.log('Connecting...')
+
+started = performance.now()
+client.connect()
