eee6041f4c49a8a13efe6bda0fb4063073a104de
lachlan <hi@pokeden.xyz>
2021-05-26 14:43:33 +0800

    add more validation

--- src/rest/endpoints.ts
+++ src/rest/endpoints.ts
@@ -774,7 +774,7 @@ The `emoji` must be [URL Encoded](https://en.wikipedia.org/wiki/Percent-encoding
    * Returns a list of [guild member](#DOCS_RESOURCES_GUILD/guild-member-object) objects that are members of the guild.
    */
   async listGuildMembers(guildId: string, params: { limit?: number, after?: string }): Promise<MemberPayload[]> {
-    if (params.limit !== undefined) {
+    if (params && (params.limit !== undefined)) {
       if (params.limit < 1 || params.limit > 1000) throw new Error('Limit should be a number between 1 and 1000')
     }
 
@@ -785,14 +785,18 @@ The `emoji` must be [URL Encoded](https://en.wikipedia.org/wiki/Percent-encoding
    * Returns a list of [guild member](#DOCS_RESOURCES_GUILD/guild-member-object) objects whose username or nickname starts with a provided string.
    */
   async searchGuildMembers(guildId: string, params: { query: string, limit?: number }): Promise<MemberPayload[]> {
-    if (params.limit !== undefined) {
-      if (params.limit < 1 || params.limit > 1000) throw new Error('Limit should be a number between 1 and 1000')
+    if (params === undefined) {
+      throw new Error('Params is not defined')
     }
 
     if (params.query === undefined) {
       throw new Error('Query is a required parameter')
     }
 
+    if (params.limit !== undefined) {
+      if (params.limit < 1 || params.limit > 1000) throw new Error('Limit should be a number between 1 and 1000')
+    }
+    
     return this.rest.get(`/guilds/${guildId}/members/search`, params)
   }
 
