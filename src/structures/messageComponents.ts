ae4297cd954a7963fe6778dbf4e513b703b6fa66
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2021-06-16 10:51:46 +0530

    fix: message being undefined on message component interaction

--- src/structures/messageComponents.ts
+++ src/structures/messageComponents.ts
@@ -82,6 +82,7 @@ export class MessageComponentInteraction extends Interaction {
     super(client, data, others)
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
     this.data = data.data as InteractionMessageComponentData
+    this.message = others.message!
   }
 
   get customID(): string {
