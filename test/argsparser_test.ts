da38ba6ae065aa031d1651cd7b542ad84365f483
MierenManz <christiaanmens@gmail.com>
2021-07-18 17:39:02 +0200

    Fix(args_parser): Fix all tests & Restructure

--- test/argsparser_test.ts
+++ test/argsparser_test.ts
@@ -1,7 +1,49 @@
 import { Args, parseArgs } from '../src/utils/command.ts'
 import { assertEquals, assertNotEquals } from './deps.ts'
+import type { Message } from "../mod.ts"
 
-const commandArgs: Args[] = [
+// Mocking of Client
+class FakeClient {
+  channels = {
+    get: function (id: string) {
+      return new FakeObj(id, "channel")
+    }
+  }
+  users = {
+    get: function (id: string) {
+      return new FakeObj(id, "user");
+    }
+  }
+}
+// Mocking of User, Channel and Role
+class FakeObj {
+  mimicClass: 'channel' | 'user' | 'roles'
+  id: string
+  constructor(id: string, mimic: 'channel' | 'user' | 'roles') {
+    this.id = id
+    this.mimicClass = mimic
+  }
+}
+// Mocking of Message
+class FakeMessage {
+  content: string;
+  client = new FakeClient();
+  guild = {
+    roles: {
+      get: function(id: string) {
+        return new FakeObj(id, 'roles')
+      }
+    }
+  }
+
+  constructor(content: string) {
+    this.content = content
+  }
+}
+
+// Command arguments needed for testing
+// Used in test 1, 2 and 3
+const commandArgs1: Args[] = [
   {
     name: 'originalMessage',
     match: 'content'
@@ -22,86 +64,7 @@ const commandArgs: Args[] = [
     defaultValue: 'ree'
   }
 ]
-
-const messageArgs1: string[] = [
-  '<@!708544768342229012>',
-  '--permanent',
-  'bye',
-  'bye',
-  'Skyler'
-]
-const expectedResult1 = {
-  originalMessage: [
-    '<@!708544768342229012>',
-    '--permanent',
-    'bye',
-    'bye',
-    'Skyler'
-  ],
-  permaban: true,
-  user: '708544768342229012',
-  reason: 'bye bye Skyler'
-}
-
-Deno.test({
-  only: false,
-  name: 'parse command arguments 1 (assertEquals)',
-  fn: () => {
-    const result = parseArgs(commandArgs, messageArgs1)
-    assertEquals(result, expectedResult1)
-  },
-  sanitizeOps: true,
-  sanitizeResources: true,
-  sanitizeExit: true
-})
-
-const messageArgs2: string[] = [
-  '<@!708544768342229012>',
-  'bye',
-  'bye',
-  'Skyler'
-]
-const expectedResult2 = {
-  originalMessage: ['<@!708544768342229012>', 'bye', 'bye', 'Skyler'],
-  permaban: true,
-  user: '708544768342229012',
-  reason: 'bye bye Skyler'
-}
-
-Deno.test({
-  name: 'parse command arguments 2 (assertEquals)',
-  fn: () => {
-    const result = parseArgs(commandArgs, messageArgs2)
-    assertEquals(result, expectedResult2)
-  },
-  sanitizeOps: true,
-  sanitizeResources: true,
-  sanitizeExit: true
-})
-
-const messageArgs3: string[] = [
-  '<@!708544768342229012>',
-  'bye',
-  'bye',
-  'Skyler'
-]
-const expectedResult3 = {
-  permaban: false,
-  user: '708544768342229012',
-  reason: 'bye bye Skyler'
-}
-
-Deno.test({
-  name: 'parse command arguments default value (assertNotEquals)',
-  fn: () => {
-    const result = parseArgs(commandArgs, messageArgs3)
-    assertNotEquals(result, expectedResult3)
-  },
-  sanitizeOps: true,
-  sanitizeResources: true,
-  sanitizeExit: true
-})
-
+// Used in test 4 and 5
 const commandArgs2: Args[] = [
   {
     name: 'user',
@@ -121,37 +84,8 @@ const commandArgs2: Args[] = [
     defaultValue: 'ree'
   }
 ]
-
-const messageArgs4: string[] = [
-  '<@!708544768342229012>',
-  'bye',
-  '<#783319033730564098>',
-  '<@&836715188690092032>'
-]
-const expectedResult4 = {
-  channel: '783319033730564098',
-  role: '836715188690092032',
-  user: '708544768342229012',
-  reason: 'bye'
-}
-
-Deno.test({
-  name: 'parse command arguments mentions (assertEquals)',
-  fn: () => {
-    const result = parseArgs(commandArgs2, messageArgs4)
-    assertEquals(result, expectedResult4)
-  },
-  sanitizeOps: true,
-  sanitizeResources: true,
-  sanitizeExit: true
-})
-
-const messageArgs5: string[] = ['<@!708544768342229012>']
-const expectedResult5 = {
-  user: '708544768342229012',
-  reason: 'No reason provided'
-}
-const commandArgs5: Args[] = [
+// Used in test 6
+const commandArgs3: Args[] = [
   {
     name: 'user',
     match: 'user'
@@ -162,47 +96,11 @@ const commandArgs5: Args[] = [
     defaultValue: 'No reason provided'
   }
 ]
-Deno.test({
-  name: 'parse command arguments, rest match default',
-  fn: () => {
-    const result = parseArgs(commandArgs5, messageArgs5)
-    assertEquals(result, expectedResult5)
-  }
-})
-
-// only ID testing
-const messageArgs7: string[] = [
-  '708544768342229012',
-  'bye',
-  '783319033730564098',
-  '836715188690092032'
-]
-const expectedResult7 = {
-  channel: '783319033730564098',
-  role: '836715188690092032',
-  user: '708544768342229012',
-  reason: 'bye'
-}
-Deno.test({
-  name: "parse command arguments with ID's (assertEquals)",
-  fn: () => {
-    const result = parseArgs(commandArgs2, messageArgs7)
-    assertEquals(result, expectedResult7)
-  },
-  sanitizeOps: true,
-  sanitizeResources: true,
-  sanitizeExit: true
-})
-
-const messageArgs6: string[] = ['get', '<@!708544768342229012>']
-const expectedResult6 = {
-  user: '708544768342229012',
-  subcommand: ['get']
-}
-const commandArgs6: Args[] = [
+// Used in test 7
+const commandArgs4: Args[] = [
   {
     name: 'user',
-    match: 'mentionUser'
+    match: 'user'
   },
   {
     name: 'subcommand',
@@ -212,10 +110,143 @@ const commandArgs6: Args[] = [
   }
 ]
 
+
+// TEST 1
+const message1 = new FakeMessage('<@!708544768342229012> --permanent bye bye Skyler')
+const expectedResult1 = {
+  originalMessage: [
+    '<@!708544768342229012>',
+    '--permanent',
+    'bye',
+    'bye',
+    'Skyler'
+  ],
+  permaban: true,
+  user: new FakeObj('708544768342229012', 'user'),
+  reason: 'bye bye Skyler'
+}
+Deno.test({
+  only: false,
+  name: 'parse command arguments 1 (assertEquals)',
+  fn: async () => {
+    const result = await parseArgs(commandArgs1, message1 as unknown as Message)
+    assertEquals(result, expectedResult1)
+  },
+  sanitizeOps: true,
+  sanitizeResources: true,
+  sanitizeExit: true
+})
+
+
+// TEST 2
+const message2 = new FakeMessage('<@!708544768342229012> bye bye Skyler')
+const expectedResult2 = {
+  originalMessage: ['<@!708544768342229012>', 'bye', 'bye', 'Skyler'],
+  permaban: true,
+  user: new FakeObj('708544768342229012', 'user'),
+  reason: 'bye bye Skyler'
+}
+Deno.test({
+  name: 'parse command arguments 2 (assertEquals)',
+  fn: async () => {
+    const result = await parseArgs(commandArgs1, message2 as unknown as Message)
+    assertEquals(result, expectedResult2)
+  },
+  sanitizeOps: true,
+  sanitizeResources: true,
+  sanitizeExit: true
+})
+
+
+// TEST 3
+const message3 = new FakeMessage('<@!708544768342229012> bye bye Skyler')
+const expectedResult3 = {
+  originalMessage: [
+    '<@!708544768342229012>',
+    'bye',
+    'bye',
+    'Skyler'
+  ],
+  // Only permaban should be different!
+  permaban: false,
+  user: new FakeObj('708544768342229012', 'user'),
+  reason: 'bye bye Skyler'
+}
+Deno.test({
+  name: 'parse command arguments default value (assertNotEquals)',
+  fn: async () => {
+    const result = await parseArgs(commandArgs1, message3 as unknown as Message)
+    assertNotEquals(result!.permaban, expectedResult3.permaban)
+
+    // This makes sure that only `expectedResult3.permaban` differs
+    assertEquals(result!.originalMessage, expectedResult3.originalMessage)
+    assertEquals(result!.user, expectedResult3.user)
+    assertEquals(result!.reason, expectedResult3.reason)
+  },
+  sanitizeOps: true,
+  sanitizeResources: true,
+  sanitizeExit: true
+})
+
+
+// TEST 4
+const message4 = new FakeMessage('<@!708544768342229012> bye <#783319033730564098> <@&836715188690092032>')
+const expectedResult4 = {
+  channel: new FakeObj('783319033730564098', 'channel'),
+  role: new FakeObj('836715188690092032', 'roles'),
+  user: new FakeObj('708544768342229012', 'user'),
+  reason: 'bye'
+}
+Deno.test({
+  name: 'parse command arguments mentions (assertEquals)',
+  fn: async () => {
+    const result = await parseArgs(commandArgs2, message4 as unknown as Message)
+    assertEquals(result, expectedResult4)
+  },
+  sanitizeOps: true,
+  sanitizeResources: true,
+  sanitizeExit: true
+})
+
+
+// TEST 5
+const message5 = new FakeMessage('708544768342229012 bye 783319033730564098 836715188690092032')
+Deno.test({
+  name: "parse command arguments with ID's (assertEquals)",
+  fn: async () => {
+    const result = await parseArgs(commandArgs2, message5 as unknown as Message)
+    assertEquals(result, expectedResult4)
+  },
+  sanitizeOps: true,
+  sanitizeResources: true,
+  sanitizeExit: true
+})
+
+
+// TEST 6
+const message6 = new FakeMessage('<@!708544768342229012>')
+const expectedResult6 = {
+  user: new FakeObj('708544768342229012', 'user'),
+  reason: 'No reason provided'
+}
+Deno.test({
+  name: 'parse command arguments, rest match default',
+  fn: async () => {
+    const result = await parseArgs(commandArgs3, message6 as unknown as Message)
+    assertEquals(result, expectedResult6)
+  }
+})
+
+// TEST 7
+const message7 = new FakeMessage('get <@!708544768342229012>')
+const expectedResult7 = {
+  user: new FakeObj('708544768342229012', 'user'),
+  subcommand: ['get']
+}
 Deno.test({
   name: 'parse command arguments, match content filter',
-  fn: () => {
-    const result = parseArgs(commandArgs6, messageArgs6)
-    assertEquals(result, expectedResult6)
+  fn: async () => {
+    const result = await parseArgs(commandArgs4, message7 as unknown as Message)
+    assertEquals(result, expectedResult7)
   }
 })
