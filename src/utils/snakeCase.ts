baa43e909c7f068357574512d2abc122370e1a3f
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2021-08-26 13:34:11 +0530

    fix(util/toCamelCase): arrays can have null smh

--- src/utils/snakeCase.ts
+++ src/utils/snakeCase.ts
@@ -8,7 +8,9 @@ export function camelCase(name: string): string {
 }
 
 export function toCamelCase(data: any): any {
-  if (Array.isArray(data)) return data.map(toCamelCase)
+  if (Array.isArray(data)) return data.map((e) => {
+    return typeof e === 'object' && e !== null ? toCamelCase(e) : e
+  })
   const result: any = {}
   Object.entries(data).forEach(([k, v]) => {
     result[camelCase(k)] =
