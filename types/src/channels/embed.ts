edbbba209e467f7c39c854e63b20385f2ca99fff
Helloyunho <yunho050840@gmail.com>
2021-09-02 22:31:23 +0900

    ✨ feat(types): add embed types

---
+++ types/src/channels/embed.ts
@@ -0,0 +1,64 @@
+export enum EmbedType {
+  RICH = "rich",
+  IMAGE = "image",
+  VIDEO = "video",
+  GIFV = "gifv",
+  ARTICLE = "article",
+  LINK = "link",
+}
+
+export interface EmbedVideoPayload {
+  url?: string;
+  proxy_url?: string;
+  height?: number;
+  width?: number;
+}
+
+export interface EmbedImagePayload {
+  url?: string;
+  proxy_url?: string;
+  height?: number;
+  width?: number;
+}
+
+export interface EmbedThumbnailPayload extends EmbedImagePayload {}
+
+export interface EmbedProviderPayload {
+  name?: string;
+  url?: string;
+}
+
+export interface EmbedAuthorPayload {
+  name?: string;
+  url?: string;
+  icon_url?: string;
+  proxy_icon_url?: string;
+}
+
+export interface EmbedFooterPayload {
+  text?: string;
+  icon_url?: string;
+  proxy_icon_url?: string;
+}
+
+export interface EmbedFieldPayload {
+  name?: string;
+  value?: string;
+  inline?: boolean;
+}
+
+export interface EmbedPayload {
+  title?: string;
+  type?: EmbedType;
+  description?: string;
+  url?: string;
+  timestamp?: string;
+  color?: number;
+  footer?: EmbedFooterPayload;
+  image?: EmbedImagePayload;
+  thumbnail?: EmbedThumbnailPayload;
+  video?: EmbedVideoPayload;
+  provider?: EmbedProviderPayload;
+  author?: EmbedAuthorPayload;
+  fields?: EmbedFieldPayload[];
+}
--- types/src/channels/mod.ts
+++ types/src/channels/mod.ts
@@ -2,3 +2,4 @@ export * from "./base.ts";
 export * from "./dm.ts";
 export * from "./guild.ts";
 export * from "./thread.ts";
+export * from "./embed.ts";
