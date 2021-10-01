1ec4bd566b852563e4f0a8fb75fd5dec477a43eb
Grian <grianguy32@gmail.com>
2021-10-01 16:26:14 +0300

    fix: forgot about unneeded workaround cleanup

--- src/commands/client.ts
+++ src/commands/client.ts
@@ -154,7 +154,6 @@ export class CommandClient extends Client implements CommandClientOptions {
 
   /** Processes a Message to Execute Command. */
   async processMessage(msg: Message): Promise<any> {
-    let commandNotFound = false;
     if (!this.allowBots && msg.author.bot === true) return
 
     const isUserBlacklisted = await this.isUserBlacklisted(msg.author.id)
