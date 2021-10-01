8fc43e6ea1b70e120ddef0096ba5defae552bb52
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2021-10-01 03:41:41 +0000

    feat: add devcontainer

---
+++ scripts/setup.sh
@@ -0,0 +1,23 @@
+echo "-> Installing Deno"
+if test -f "/home/codespace/.deno/bin/deno"; then
+  echo "Deno already installed."
+else
+  curl -fsSL https://deno.land/x/install/install.sh | sh
+  echo "-> Add Deno to PATH"
+  echo "export DENO_INSTALL=\"/home/codespace/.deno/bin\"
+export PATH=\"\$DENO_INSTALL:\$PATH\"" >> ~/.bashrc
+fi
+
+echo "-> Setup VS Code Config"
+if test -f "./.vscode/settings.json"; then
+  echo "Skipping VS Code Config, .vscode/settings.json already exists."
+else
+  mkdir .vscode
+  echo "{
+  \"deno.path\": \"/home/codespace/.deno/bin/deno\",
+  \"deno.enable\": true,
+  \"editor.tabSize\": 2,
+  \"editor.defaultFormatter\": \"esbenp.prettier-vscode\",
+  \"editor.formatOnSave\": true
+}" > .vscode/settings.json
+fi
