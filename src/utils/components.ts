81c8910913b4b7c2d456b351721f09f0f62b1507
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2021-10-04 11:17:27 +0000

    don't use _

--- src/utils/components.ts
+++ src/utils/components.ts
@@ -9,8 +9,8 @@ import { toCamelCase } from '../utils/snakeCase.ts'
 export function transformComponent(
   d: MessageComponentData[]
 ): MessageComponentPayload[] {
-  return d.map((_: unknown) => {
-    const e = { ...(_ as Record<string, unknown>) }
+  return d.map((data: unknown) => {
+    const e = { ...(data as Record<string, unknown>) }
     if (e.customID !== undefined) {
       e.custom_id = e.customID
       delete e.customID
