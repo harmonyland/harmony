89ea84a3a0b2b08f51189d0348fd7a3945745469
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2021-10-06 10:47:37 +0000

    feat(application): update types and structure

--- src/structures/application.ts
+++ src/structures/application.ts
@@ -1,8 +1,15 @@
 import type { Client } from '../client/mod.ts'
-import type { ApplicationPayload } from '../types/application.ts'
+import { ApplicationFlags, ApplicationPayload } from '../types/application.ts'
 import { SnowflakeBase } from './base.ts'
 import { User } from './user.ts'
 import { Team } from './team.ts'
+import { BitField, BitFieldResolvable } from '../utils/bitfield.ts'
+
+export class ApplicationFlagsManager extends BitField {
+  constructor(bits: BitFieldResolvable | undefined) {
+    super(ApplicationFlags, bits ?? 0)
+  }
+}
 
 export class Application extends SnowflakeBase {
   id: string
@@ -12,6 +19,18 @@ export class Application extends SnowflakeBase {
   summary: string
   bot?: User
   team?: Team
+  rpcOrigins?: string[]
+  botPublic?: boolean
+  botRequireCodeGrant?: boolean
+  termsOfServiceURL?: string
+  privacyPolicyURL?: string
+  owner?: User
+  verifyKey?: string
+  guildID?: string
+  primarySkuID?: string
+  slug?: string
+  coverImage?: string
+  flags: ApplicationFlagsManager
 
   constructor(client: Client, data: ApplicationPayload) {
     super(client, data)
@@ -23,5 +42,18 @@ export class Application extends SnowflakeBase {
     this.summary = data.summary
     this.bot = data.bot !== undefined ? new User(client, data.bot) : undefined
     this.team = data.team !== null ? new Team(client, data.team) : undefined
+    this.rpcOrigins = data.rpc_origins
+    this.botPublic = data.bot_public
+    this.botRequireCodeGrant = data.bot_require_code_grant
+    this.termsOfServiceURL = data.terms_of_service_url
+    this.privacyPolicyURL = data.privacy_policy_url
+    this.owner =
+      data.owner !== undefined ? new User(client, data.owner) : undefined
+    this.verifyKey = data.verify_key
+    this.guildID = data.guild_id
+    this.primarySkuID = data.primary_sku_id
+    this.slug = data.slug
+    this.coverImage = data.cover_image
+    this.flags = new ApplicationFlagsManager(data.flags)
   }
 }
