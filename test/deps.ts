bf53bc6dc7ddf1a2d210ffb08fcf12df069410e5
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2022-01-15 17:47:12 +0530

    update test deps

--- test/deps.ts
+++ test/deps.ts
@@ -1,2 +1,2 @@
-export * from 'https://deno.land/std@0.95.0/testing/asserts.ts'
-export * from 'https://deno.land/std@0.95.0/http/server.ts'
+export * from 'https://deno.land/std@0.121.0/testing/asserts.ts'
+export * from 'https://deno.land/std@0.121.0/http/server.ts'
