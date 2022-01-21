6e43ac9712bea3d66eae31338052076b47976910
Helloyunho <yunho050840@gmail.com>
2022-01-22 08:30:48 +0900

    🏷️ feat(sticker): add every sticker types

---
+++ types/src/stickers/sticker.ts
@@ -0,0 +1,57 @@
+export interface StickerPayload {
+  id: string;
+  pack_id?: string;
+  name: string;
+  description: string | null;
+  tags: string;
+  type: StickerType;
+  format_type: StickerFormatType;
+  available?: boolean;
+  guild_id?: string;
+  // user?: UserPayload
+  sort_value?: number;
+}
+
+export enum StickerType {
+  STANDARD = 1,
+  GUILD = 2,
+}
+
+export enum StickerFormatType {
+  PNG = 1,
+  APNG = 2,
+  LOTTIE = 3,
+}
+
+export interface StickerItemPayload {
+  id: string;
+  name: string;
+  format_type: StickerFormatType;
+}
+
+export interface StickerPackPayload {
+  id: string;
+  stickers: StickerPayload[];
+  name: string;
+  sku_id: string;
+  cover_sticker_id?: string;
+  description: string;
+  banner_asset_id?: string;
+}
+
+export interface ListNitroStickerPacksPayload {
+  sticker_packs: StickerPackPayload[];
+}
+
+export interface CreateGuildStickerPayload {
+  name: string;
+  description: string;
+  tags: string;
+  file: Blob;
+}
+
+export interface EditGuildStickerPayload {
+  name?: string;
+  description?: string;
+  tags?: string;
+}
