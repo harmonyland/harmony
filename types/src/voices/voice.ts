3543c393a4cee3f142e4476f3eb7283a6e27b57d
Helloyunho <yunho050840@gmail.com>
2022-01-22 05:07:57 +0000

    🏷️ feat(voice): add every voice types

---
+++ types/src/voices/voice.ts
@@ -0,0 +1,23 @@
+import { GuildMemberPayload } from "../guilds/member.ts";
+
+export interface VoiceStatePayload {
+  guild_id?: string;
+  channel_id: string;
+  user_id: string;
+  member?: GuildMemberPayload;
+  session_id: string;
+  deaf: boolean;
+  mute: boolean;
+  self_deaf: boolean;
+  self_mute: boolean;
+  suppress: boolean;
+  request_to_speak_timestamp: string | null;
+}
+
+export interface VoiceRegionPayload {
+  id: string;
+  name: string;
+  optimal: boolean;
+  deprecated: boolean;
+  custom: boolean;
+}
