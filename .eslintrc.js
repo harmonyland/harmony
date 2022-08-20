addaee73a2701ea7b37b1f32fa79b061b5816763
Dj <43033058+DjDeveloperr@users.noreply.github.com>
2022-08-20 04:10:53 +0000

    x

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['standard-with-typescript', 'prettier', 'prettier/standard'],
+  extends: ['standard-with-typescript', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
