4c0b4786e3aac4d713da9f3a8b93deabc6092d9b
Helloyunho <yunho050840@gmail.com>
2022-11-25 21:06:55 +0900

    ⚗️ feat: make sharded gateway more advanced

--- core/src/gateway/sharded.ts
+++ core/src/gateway/sharded.ts
@@ -3,23 +3,25 @@ import { RESTClient } from "../rest/rest_client.ts";
 import { Gateway, GatewayOptions } from "./mod.ts";
 
 export interface ShardedGatewayOptions extends Omit<GatewayOptions, "shard"> {
-  shards: number;
+  shards?: number;
 }
 
 export class ShardedGateway {
-  shards: Gateway[] = [];
+  shards: Record<number, Gateway> = {};
   token: string;
   intents: number;
-  options: GatewayOptions;
+  options: ShardedGatewayOptions;
+  shardCount?: number;
 
   constructor(
     token: string,
     intents: number,
-    options = {},
+    options: ShardedGatewayOptions = {},
   ) {
     this.token = token;
     this.intents = intents;
     this.options = options;
+    this.shardCount = options.shards;
   }
 
   async getBestShardCount() {
@@ -31,15 +33,35 @@ export class ShardedGateway {
     return shards;
   }
 
-  async spawn() {
+  async getShardCount() {
+    if (this.shardCount) return this.shardCount;
     const shardCount = await this.getBestShardCount();
+    this.shardCount = shardCount;
+    return shardCount;
+  }
+
+  async spawn(shardID: number) {
+    const shardCount = await this.getShardCount();
+    this.shards[shardID] = new Gateway(this.token, this.intents, {
+      ...this.options,
+      shard: [shardID, shardCount],
+    });
+  }
+
+  async spawnAll() {
+    const shardCount = await this.getShardCount();
     for (let i = 0; i < shardCount; i++) {
-      this.shards.push(
-        new Gateway(this.token, this.intents, {
-          ...this.options,
-          shard: [i, shardCount],
-        }),
-      );
+      await this.spawn(i);
+    }
+  }
+
+  async destroy(shardID: number) {
+    await this.shards[shardID].disconnect();
+  }
+
+  async destroyAll() {
+    for (const shardID in this.shards) {
+      await this.destroy(Number(shardID));
     }
   }
 }
