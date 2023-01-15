5d65e85223dbf3e86d6253d82e49ba3e26999d1a
Helloyunho <yunho050840@gmail.com>
2023-01-15 16:27:54 +0900

    🏷️ fix: some files are not exported

--- core/mod.ts
+++ core/mod.ts
@@ -1,1 +1,5 @@
 export * from "./src/rest/mod.ts";
+export * from "./src/gateway/mod.ts";
+export * from "./src/gateway/sharded.ts";
+export * from "./src/manager.ts";
+export * from "./types/gateway.ts";
