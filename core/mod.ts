3ab75610a810b71a62b3003ee27c8889a66cf382
Helloyunho <yunho050840@gmail.com>
2023-01-15 16:45:42 +0900

    🏷️ fix: how-

--- core/mod.ts
+++ core/mod.ts
@@ -2,4 +2,4 @@ export * from "./src/rest/mod.ts";
 export * from "./src/gateway/mod.ts";
 export * from "./src/gateway/sharded.ts";
 export * from "./src/manager.ts";
-export * from "./types/gateway.ts";
+export * from "./types/gateway/events.ts";
