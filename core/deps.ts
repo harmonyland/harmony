f7980148dfea69c6e1394053a3d19b6a19d328b8
Helloyunho <yunho050840@gmail.com>
2024-05-19 00:46:58 +0900

    ⬆️ chore(core): upgrade deps

--- core/deps.ts
+++ core/deps.ts
@@ -1,2 +1,2 @@
-export { decompress_with as unzlib } from "https://unpkg.com/@evan/wasm@0.0.50/target/zlib/deno.js";
-export { EventEmitter } from "https://deno.land/x/event@2.0.0/mod.ts";
+export { decompress_with as unzlib } from "npm:@evan/wasm@0.0.95/target/zlib/node.mjs";
+export { EventEmitter } from "jsr:@denosaurs/event@2.0.2";
