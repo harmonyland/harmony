5a1d3cdc10df276cae52919fca8121fbb3aebf96
Helloyunho <yunho050840@gmail.com>
2024-05-19 00:46:37 +0900

    ⬆️ chore(framework): upgrade deps

--- framework/deps.ts
+++ framework/deps.ts
@@ -1,2 +1,2 @@
-export { EventEmitter } from "https://deno.land/x/event@2.0.1/mod.ts";
-export { Mixin } from "https://esm.sh/ts-mixer@6.0.3";
+export { EventEmitter } from "jsr:@denosaurs/event@2.0.2";
+export { Mixin } from "npm:ts-mixer@6.0.4";
